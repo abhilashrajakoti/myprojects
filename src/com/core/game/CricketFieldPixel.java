51269a73b8719724cd45f21e9836beb3ac412bb3
Abhilash Rajakoti <abhi.delfiten@gmail.com>
2017-09-24 23:14:58 +0530

    initial commit gameengine core files

---
+++ src/com/core/game/CricketFieldPixel.java
@@ -0,0 +1,19 @@
+package com.core.game;
+
+public class CricketFieldPixel extends CricketGround {
+private int id;
+private String name;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
+}
---
+++ src/com/core/game/CricketGround.java
@@ -0,0 +1,13 @@
+package com.core.game;
+
+public class CricketGround {
+private String name;
+
+public String getGroundName() {
+	return name;
+}
+
+public void setGroundName(String name) {
+	this.name = name;
+}
+}
